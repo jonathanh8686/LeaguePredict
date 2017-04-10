cae56604a1c28fcf15da267a391cfa275da7fd5a
Jonathan Hsieh <jonathanh1386@gmail.com>
2017-04-10 15:53:28 -0700

    Resharper cleaned up code

--- LeaguePredict/GetRawGames.cs
+++ LeaguePredict/GetRawGames.cs
@@ -5,30 +5,31 @@ using System.Threading;
 
 namespace LeaguePredict
 {
-    static class GetRawGames
+    internal static class GetRawGames
     {
-        static readonly string apiKey = "";
+        private static readonly string apiKey = "";
 
         /// <summary>
-        /// Returns the recent games data from Riot
+        ///     Returns the recent games data from Riot
         /// </summary>
         /// <param name="playerId">The ID of the player who's recent games to return</param>
         /// <returns>Raw Recent Game Data</returns>
         public static string GetRecentGames(string playerId)
         {
-            string getstr = "https://na.api.riotgames.com/api/lol/NA/v1.3/game/by-summoner/" + playerId + "/recent?api_key=" + apiKey;
+            string getstr = "https://na.api.riotgames.com/api/lol/NA/v1.3/game/by-summoner/" + playerId +
+                            "/recent?api_key=" + apiKey;
             return new WebClient().DownloadString(getstr);
         }
 
         /// <summary>
-        /// Loops through Riot API and gets >30lvl ID's
-        /// A one time / not often run
+        ///     Loops through Riot API and gets >30lvl ID's
+        ///     A one time / not often run
         /// </summary>
         /// <returns>ID's of people >lvl30</returns>
         public static List<string> GetUserIDs(int min = 57067600, int max = 57467600)
         {
-            List<string> eligUsers = new List<string>();
-            WebClient wc = new WebClient();
+            var eligUsers = new List<string>();
+            var wc = new WebClient();
             string getIds = "";
             for (int i = min; i < max; i++)
             {
@@ -36,7 +37,8 @@ namespace LeaguePredict
                 if ((i - min) % 40 != 0 || i == min) continue;
 
                 getIds = getIds.TrimEnd(',');
-                string getstr = "https://na.api.riotgames.com/api/lol/NA/v1.4/summoner/" + getIds + "?api_key=" + apiKey;
+                string getstr = "https://na.api.riotgames.com/api/lol/NA/v1.4/summoner/" + getIds + "?api_key=" +
+                                apiKey;
                 string rawdata;
                 try
                 {
